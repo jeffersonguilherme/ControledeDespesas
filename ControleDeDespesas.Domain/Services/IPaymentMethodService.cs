aba09450142ed764899553290619bc0e45e90c8c
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2025-12-27 21:59:01 -0300

    feat: Adiciona IPaymentMethodService, PaymentMethodService e GetByName em IPaymentMethodRepository
    - Criada interface IPaymentMethodService para operações de negócio de métodos de pagamento
    - Implementada classe PaymentMethodService consumindo IPaymentMethodRepository
    - Adicionado método GetByName na interface IPaymentMethodRepository

--- ControleDeDespesas.Domain/Services/IPaymentMethodService.cs
+++ ControleDeDespesas.Domain/Services/IPaymentMethodService.cs
@@ -1,13 +1,12 @@
 using ControleDeDespesas.Domain.Models;
 
-namespace ControleDeDespesas.Domain.Repositories.Interfaces;
+namespace ControleDeDespesas.Domain.Services;
 
-public interface IPaymentMethodRepository
+public interface IPaymentMethodService
 {
+    Task AddAsync(PaymentMethod paymentMethod);
     Task<PaymentMethod> GetByIdAsync(Guid id);
     Task<IEnumerable<PaymentMethod>> GetAllAsync();
-    Task AddAsync(PaymentMethod paymentMethod);
     Task UpdateAsync(PaymentMethod paymentMethod);
     Task DeleteAsync(Guid id);
-    Task<PaymentMethod> GetByNameAsync(string name);
 }
