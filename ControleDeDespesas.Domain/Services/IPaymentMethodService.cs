79421eb6a889dbc532873829d1679ad01aae42b5
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2026-01-03 23:07:08 -0300

    feat(payment-method): implementa repositório e serviço e adiciona busca por nome
    - Implementa PaymentMethodRepository
    - Implementa PaymentMethodService
    - Adiciona método GetByNameAsync na interface IPaymentMethodService

--- ControleDeDespesas.Domain/Services/IPaymentMethodService.cs
+++ ControleDeDespesas.Domain/Services/IPaymentMethodService.cs
@@ -7,6 +7,7 @@ public interface IPaymentMethodService
     Task AddAsync(PaymentMethod paymentMethod);
     Task<PaymentMethod> GetByIdAsync(Guid id);
     Task<IEnumerable<PaymentMethod>> GetAllAsync();
+    Task<PaymentMethod> GetByNameAsync(string name);
     Task UpdateAsync(PaymentMethod paymentMethod);
     Task DeleteAsync(Guid id);
 }
